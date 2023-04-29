4fd94b6c535c6310b382eca48094b8d4007a507d
Darren Key <dyk34@cornell.edu>
2023-04-29 12:50:31 -0400

    First commit

---
+++ demo.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+# Titling
+
+# Streamlit writing
+
+# Streamlit magic
+
+# Sliders + widget updating
+
+# pandas integration
+
+# charts
+
+
+# checkbox
+
+# sidebar
